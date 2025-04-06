2566cef993b0d7e112454036a7454e5f9685ecfc
Muhammad <hamzaadil56@gmail.com>
2025-04-06 16:34:41 +0500

    added shadcn button and enter key button

--- components/layout/Hero.tsx
+++ components/layout/Hero.tsx
@@ -8,10 +8,9 @@ import Image from "next/image";
 import HumanMessageBox from "../shared/HumanMessageBox";
 import AIMessageBox from "../shared/AIMessageBox";
 import { useChat } from "@ai-sdk/react";
+import { Button } from "@/components/ui/button";
 
 const Hero = () => {
-	const [message, setMessage] = useState("");
-
 	const textareaRef = useRef<HTMLTextAreaElement | null>(null);
 	const { messages, input, handleInputChange, handleSubmit } = useChat({
 		maxSteps: 3,
@@ -29,11 +28,18 @@ const Hero = () => {
 
 	useEffect(() => {
 		adjustTextareaHeight();
-	}, [message]);
+	}, [input]);
+
+	const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+		if (e.key === "Enter" && !e.shiftKey) {
+			e.preventDefault();
+			handleSubmit(e as unknown as React.FormEvent<HTMLFormElement>);
+		}
+	};
 
 	return (
-		<div className="flex flex-col items-center justify-center px-4 py-6  w-full">
-			<h1 className="text-4xl w-full  text-center text-light-gray">
+		<div className="flex flex-col items-center justify-center px-4 py-6 w-full">
+			<h1 className="text-4xl w-full text-center text-light-gray">
 				Ask anything about me from my AI assistant!
 			</h1>
 
@@ -65,27 +71,21 @@ const Hero = () => {
 				animate={{ opacity: 1, y: 0 }}
 				transition={{ duration: 0.3 }}
 				onSubmit={handleSubmit}
-				className="w-full max-w-3xl mx-auto  bg-secondary-gray rounded-lg mt-6"
+				className="w-full max-w-3xl mx-auto bg-secondary-gray rounded-lg mt-6"
 			>
-				<div className="flex flex-row bg-secondary-gray p-4 rounded-lg scrollbar-hide  shadow-md overflow-hidden">
+				<div className="flex flex-row bg-secondary-gray p-4 rounded-lg scrollbar-hide shadow-md overflow-hidden">
 					<textarea
 						ref={textareaRef}
 						placeholder="Ask me anything..."
 						value={input}
 						onChange={handleInputChange}
-						className="w-full min-h-[100px] bg-secondary-gray scrollbar-hide border-none outline-none resize-none text-light-gray "
+						onKeyDown={handleKeyDown}
+						className="w-full min-h-[100px] bg-secondary-gray scrollbar-hide border-none outline-none resize-none text-light-gray"
 					/>
-					<button
-						type="submit"
-						className={`h-10 px-4 rounded-full flex items-center transition shadow-md opacity-90 justify-center gap-2 transition-colors transition-all bg-main-gray hover:opacity-100${
-							message.trim()
-								? "bg-main-gray text-light-gray hover:opacity-100 "
-								: "bg-gray-200 text-gray-400 "
-						}`}
-					>
-						<span>Send</span>
+					<Button type="submit">
+						Send
 						<Send size={16} />
-					</button>
+					</Button>
 				</div>
 			</motion.form>
 		</div>
